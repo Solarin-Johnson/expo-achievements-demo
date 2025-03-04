5778111be6b8e495efe5db60e3c3d6fe26a5b30e
Solarin-Johnson <solarinjohnson@gmail.com>
2025-03-04 15:52:53 +0100

    Update assets and styles: add new images, modify color constants, and implement hex to RGB conversion function

--- app/index.tsx
+++ app/index.tsx
@@ -1,49 +1,62 @@
 import {
-  Platform,
   Pressable,
   ScrollView,
   StyleSheet,
-  Text,
   useWindowDimensions,
   View,
 } from "react-native";
-import React, { useMemo, useRef, useState } from "react";
+import React, { useMemo, useRef } from "react";
 import { ThemedView } from "@/components/ThemedView";
 import { generalStyles } from "@/constants/styles";
 import { ThemedText } from "@/components/ThemedText";
 import Animated, {
   Extrapolation,
   interpolate,
-  runOnJS,
+  interpolateColor,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
 } from "react-native-reanimated";
 import { Image } from "expo-image";
-import { ACHIEVMENTS } from "@/constants";
+import { ACHIEVMENTS, hexToRgb } from "@/constants";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 export default function Index() {
   const { width } = useWindowDimensions();
   const wide = width > 540;
   const inset = useSafeAreaInsets();
-  const [currentIndex, setCurrentIndex] = useState(0);
   const text = useThemeColor({}, "text");
   const scrollRef = useRef<Animated.ScrollView>(null);
   const scrollX = useSharedValue(0);
 
-  const { title, description, color, date, completed } = useMemo(() => {
-    return ACHIEVMENTS[currentIndex];
-  }, [currentIndex]);
+  const hexColors = useMemo(
+    () =>
+      ACHIEVMENTS.map((item) =>
+        item.completed ? hexToRgb(item.color) : "rgba(0,0,0,0)"
+      ),
+    [ACHIEVMENTS]
+  );
 
   const scrollHandler = useAnimatedScrollHandler({
-    onScroll: (event: any) => {
-      const current = Math.round(scrollX.value / width);
+    onScroll: (event) => {
       scrollX.value = event.contentOffset.x;
-      runOnJS(setCurrentIndex)(current);
     },
+  });
+
+  const animatedBg = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        scrollX.value,
+        ACHIEVMENTS.map((_, i) => i * width),
+        hexColors
+      ),
+    };
   });
 
   const Badge = useMemo(() => {
@@ -54,7 +67,7 @@ export default function Index() {
         (index + 1) * width,
       ];
 
-      const opacity = useAnimatedStyle(() => {
+      const animatedValues = useDerivedValue(() => {
         return {
           opacity: interpolate(
             scrollX.value,
@@ -62,46 +75,234 @@ export default function Index() {
             [0.3, 1, 0.3],
             Extrapolation.CLAMP
           ),
+          translateX: interpolate(
+            scrollX.value,
+            inputRange,
+            [width * -0.4, 0, width * 0.4],
+            Extrapolation.CLAMP
+          ),
+          scale: interpolate(
+            scrollX.value,
+            inputRange,
+            [0.65, 1, 0.65],
+            Extrapolation.CLAMP
+          ),
+        };
+      });
+
+      const animatedStyle = useAnimatedStyle(() => {
+        return {
+          opacity: animatedValues.value.opacity,
           transform: [
-            {
-              translateX: interpolate(
-                scrollX.value,
-                inputRange,
-                [width * -0.4, 0, width * 0.4],
-                Extrapolation.CLAMP
-              ),
-            },
-            {
-              scale: interpolate(
-                scrollX.value,
-                inputRange,
-                [0.65, 1, 0.65],
-                Extrapolation.CLAMP
-              ),
-            },
+            { translateX: animatedValues.value.translateX },
+            { scale: animatedValues.value.scale },
           ],
+        };
+      });
+
+      const scrollToNext = () => {
+        if (scrollRef.current) {
+          const nextIndex = index === ACHIEVMENTS.length - 1 ? 0 : index + 1;
+          scrollRef.current.scrollTo({
+            x: width * nextIndex,
+            animated: true,
+          });
+        }
+      };
+
+      return (
+        <Pressable
+          onPress={scrollToNext}
+          hitSlop={{
+            top: 100,
+            bottom: 100,
+            left: 20,
+            right: 20,
+          }}
+        >
+          <Animated.View
+            style={[
+              styles.achievment,
+              { width: width },
+              animatedStyle,
+              {
+                filter: item.completed ? "none" : "grayscale(1)",
+              },
+            ]}
+          >
+            <View
+              style={{
+                filter: item.completed
+                  ? "none"
+                  : "brightness(0.6) grayscale(8)",
+              }}
+            >
+              <Image
+                source={item.image}
+                style={[
+                  styles.image,
+                  {
+                    width: wide ? 240 : 205,
+                  },
+                ]}
+              />
+            </View>
+            {!item.completed && (
+              <ThemedText
+                style={{
+                  lineHeight: 84,
+                  position: "absolute",
+                  transform: [{ translateY: "70%" }],
+                  filter: "drop-shadow(0 0 4px #00000040)",
+                }}
+              >
+                <MaterialIcons name="lock" size={80} />
+              </ThemedText>
+            )}
+          </Animated.View>
+        </Pressable>
+      );
+    };
+  }, [width, scrollX]);
+
+  const Content = useMemo(() => {
+    return ({ item, index }: { index: number; item: any }) => {
+      const inputRange = [
+        (index - 1) * width,
+        (index - 1) * width + width / 2.5,
+        index * width,
+        (index + 1) * width - width / 2,
+        (index + 1) * width,
+      ];
+
+      const zIndex = useAnimatedStyle(() => {
+        const value = scrollX.value;
+        return {
+          zIndex: value > inputRange[0] && value < inputRange[-1] ? 1 : 0,
+        };
+      });
+      const opacity = useAnimatedStyle(() => {
+        const value = scrollX.value;
+        return {
+          opacity: interpolate(
+            value,
+            inputRange,
+            [0, 0, 1, 0, 0],
+            Extrapolation.CLAMP
+          ),
+        };
+      });
+
+      const btnOpacity = useAnimatedStyle(() => {
+        return {
+          opacity: interpolate(
+            scrollX.value,
+            inputRange,
+            [0, 0.5, 1, 0.5, 0],
+            Extrapolation.CLAMP
+          ),
         };
       });
 
       return (
         <Animated.View
           style={[
-            styles.achievment,
-            { width: width },
-            opacity,
+            styles.content,
             {
               filter: item.completed ? "none" : "grayscale(1)",
             },
+            zIndex,
           ]}
         >
-          <Image
-            source={item.image}
-            style={{ width: wide ? 240 : 205, aspectRatio: 1 }}
-          />
+          <Animated.View
+            style={[
+              opacity,
+              {
+                gap: 16,
+              },
+            ]}
+          >
+            <ThemedText style={[generalStyles.family_text, generalStyles.text]}>
+              {item.title}
+            </ThemedText>
+            <ThemedText
+              style={[
+                generalStyles.family_sub,
+                generalStyles.sub_text,
+                {
+                  opacity: 0.7,
+                  paddingBottom: 16,
+                },
+              ]}
+            >
+              {item.description}
+            </ThemedText>
+            {item.completed && (
+              <ThemedText
+                style={[
+                  generalStyles.family_text,
+                  generalStyles.sub_text,
+                  {
+                    textTransform: "capitalize",
+                    letterSpacing: 1,
+                    fontSize: 13,
+                    opacity: 0.5,
+                  },
+                ]}
+              >
+                {`Earned on ${item.date}`}
+              </ThemedText>
+            )}
+          </Animated.View>
+
+          {item.completed && (
+            <View
+              style={[
+                generalStyles.fill,
+                {
+                  justifyContent: "flex-end",
+                  marginBottom: 24,
+                },
+              ]}
+            >
+              <AnimatedPressable
+                style={[
+                  styles.button,
+                  btnOpacity,
+                  {
+                    backgroundColor: text + 20,
+                    borderColor: text + 30,
+                  },
+                ]}
+                onPress={() => {
+                  console.log("Share", index);
+                }}
+              >
+                <ThemedText>
+                  <Feather name="share" size={15} />
+                </ThemedText>
+                <ThemedText
+                  style={[
+                    generalStyles.family_text,
+                    generalStyles.sub_text,
+                    {
+                      textTransform: "uppercase",
+                      letterSpacing: 1,
+                      fontSize: 12,
+                      opacity: 0.7,
+                      paddingHorizontal: 0,
+                    },
+                  ]}
+                >
+                  Share
+                </ThemedText>
+              </AnimatedPressable>
+            </View>
+          )}
         </Animated.View>
       );
     };
-  }, [width, scrollX]);
+  }, [scrollX, width, text, ACHIEVMENTS]);
 
   return (
     <ThemedView
@@ -113,6 +314,7 @@ export default function Index() {
         },
       ]}
     >
+      <Animated.View style={[styles.overlay, animatedBg]} />
       <ScrollView
         style={{
           maxWidth: width,
@@ -136,82 +338,21 @@ export default function Index() {
           contentContainerStyle={styles.scroll}
           onScroll={scrollHandler}
           pagingEnabled={true}
-          scrollEventThrottle={16}
           disableIntervalMomentum
         >
           {ACHIEVMENTS.map((item, index) => (
             <Badge item={item} index={index} key={index} />
           ))}
         </Animated.ScrollView>
-        <View style={styles.content}>
-          <ThemedText style={[generalStyles.family_text, generalStyles.text]}>
-            {title}
-          </ThemedText>
-          <ThemedText
-            style={[
-              generalStyles.family_sub,
-              generalStyles.sub_text,
-              {
-                paddingBottom: 16,
-              },
-            ]}
-          >
-            {description}
-          </ThemedText>
-          {completed && (
-            <>
-              <ThemedText
-                style={[
-                  generalStyles.family_text,
-                  generalStyles.sub_text,
-                  {
-                    textTransform: "capitalize",
-                    letterSpacing: 1,
-                    fontSize: 13,
-                    opacity: 0.5,
-                  },
-                ]}
-              >
-                {`Earned on ${date}`}
-              </ThemedText>
-
-              <View
-                style={[
-                  generalStyles.fill,
-                  {
-                    justifyContent: "flex-end",
-                    minHeight: 60,
-                    marginBottom: 24,
-                  },
-                ]}
-              >
-                <Pressable
-                  style={[
-                    styles.button,
-                    {
-                      backgroundColor: text + 18,
-                      borderColor: text + 24,
-                    },
-                  ]}
-                >
-                  <ThemedText
-                    style={[
-                      generalStyles.family_text,
-                      generalStyles.sub_text,
-                      {
-                        textTransform: "uppercase",
-                        letterSpacing: 1,
-                        fontSize: 12,
-                        opacity: 0.5,
-                      },
-                    ]}
-                  >
-                    Share
-                  </ThemedText>
-                </Pressable>
-              </View>
-            </>
-          )}
+        <View
+          style={{
+            flex: 1.3,
+            width,
+          }}
+        >
+          {ACHIEVMENTS.map((item, index) => (
+            <Content item={item} index={index} key={index} />
+          ))}
         </View>
       </ScrollView>
     </ThemedView>
@@ -229,14 +370,36 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     paddingHorizontal: 32,
+    paddingVertical: 24,
     gap: 16,
     alignItems: "center",
+    position: "absolute",
+    minHeight: 300,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
   button: {
     paddingVertical: 8,
-    paddingHorizontal: 16,
+    paddingHorizontal: 20,
     borderRadius: 50,
     borderWidth: 1,
-    marginBottom: Platform.OS === "web" ? 16 : 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    filter: "opacity(0.4)",
+  },
+  image: {
+    aspectRatio: 1,
+    filter: "drop-shadow(0 0 4px #00000035)",
   },
 });
--- constants/index.ts
+++ constants/index.ts
@@ -63,3 +63,18 @@ export const ACHIEVMENTS = [
     color: "#001171",
   },
 ];
+
+export const hexToRgb = (hex: string) => {
+  hex = hex.replace(/^#/, "");
+  if (hex.length === 3) {
+    hex = hex
+      .split("")
+      .map((char) => char + char)
+      .join("");
+  }
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `rgb(${r}, ${g}, ${b})`;
+};
